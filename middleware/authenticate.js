eb62d1c21c4eef68e87b812bc26165a2613141dc
Blake Yeboah <blakekyeboah@gmail.com>
2021-04-13 14:49:35 +1000

    Created Authenticate Middleware for private routes

---
+++ middleware/authenticate.js
@@ -0,0 +1,35 @@
+module.exports = (req, res, next) => {
+   const secret = process.env.JWT_SECRET;
+
+   const authHeader = req.headers.authorization;
+
+   if (authHeader) {
+      const token = authHeader.split("Bearer ")[1];
+
+      if (token) {
+         try {
+            const user = jwt.verify(token, secret);
+            req.user = user;
+            next();
+         } catch (err) {
+            return res.status(401).json({
+               errors: {
+                  authentication: "Invalid/Expired token",
+               },
+            });
+         }
+      } else {
+         return res.status(401).json({
+            errors: {
+               authentication: "Authentication token must be 'Bearer [token]'",
+            },
+         });
+      }
+   } else {
+      return res.status(401).json({
+         errors: {
+            authentication: "Must supply a jwt token in authorization header",
+         },
+      });
+   }
+};

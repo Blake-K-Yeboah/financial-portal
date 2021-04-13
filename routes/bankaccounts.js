8e6c4e633d74e6462d794d376ef1235a81ff574e
Blake Yeboah <blakekyeboah@gmail.com>
2021-04-13 21:11:53 +1000

    Added /api/bankaccounts/:id route

--- routes/bankaccounts.js
+++ routes/bankaccounts.js
@@ -10,10 +10,18 @@ const BankAccountsController = require("../controllers/BankAccountsController");
 // Authenticate Middleware
 const authenticate = require("../middleware/authenticate");
 
+// Check ID Middleware
+const checkId = require("../middleware/checkId");
+
 // @route GET api/bankaccounts
 // @desc Get all bank accounts
 // @access Private
 router.get("/", authenticate, BankAccountsController.getAll);
+
+// @route GET api/bankaccounts/:id
+// @desc Get bank account by ID
+// @access Private
+router.get("/:id", authenticate, checkId, BankAccountsController.getById);
 
 // Export Router
 module.exports = router;

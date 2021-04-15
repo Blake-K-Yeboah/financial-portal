dd8e8e86f385a4c0de1ea4f961c04bfd44b1cdeb
Blake Yeboah <blakekyeboah@gmail.com>
2021-04-15 19:32:05 +1000

    Added update (PUT) and delete routes for transactions

--- routes/transactions.js
+++ routes/transactions.js
@@ -10,10 +10,33 @@ const TransactionsController = require("../controllers/TransactionsController");
 // Authenticate Middleware
 const authenticate = require("../middleware/authenticate");
 
+// Check ID Middleware
+const checkId = require("../middleware/checkId");
+
 // @route POST api/transactions/create
 // @desc Create a transaction
 // @access Private
 router.post("/create", authenticate, TransactionsController.createTransaction);
+
+// @route PUT api/transactions/:id
+// @desc Update a transaction
+// @access Private
+router.put(
+   "/:id",
+   authenticate,
+   checkId,
+   TransactionsController.updateTransaction
+);
+
+// @route DELETE api/transactions/:id
+// @desc Delete a transaction
+// @access Private
+router.delete(
+   "/:id",
+   authenticate,
+   checkId,
+   TransactionsController.deleteTransaction
+);
 
 // Export Router
 module.exports = router;

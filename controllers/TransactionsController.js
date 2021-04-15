7d2e762767d16ec30bb2610af6a6f1307658a1d8
Blake Yeboah <blakekyeboah@gmail.com>
2021-04-15 19:31:49 +1000

    Added update and delete transaction handlers

--- controllers/TransactionsController.js
+++ controllers/TransactionsController.js
@@ -44,6 +44,84 @@ const createTransaction = async (req, res) => {
    }
 };
 
+// Edit Transaction
+const updateTransaction = async (req, res) => {
+   const { errors, isValid } = validateTransactionInput(req.body);
+
+   if (!isValid) {
+      return res.status(400).json({ errors });
+   }
+
+   const bankAccount = await BankAccount.findById(req.body.bankId);
+
+   if (!bankAccount) {
+      return res
+         .status(404)
+         .json({ errors: { account: "No bank account with that id." } });
+   }
+
+   if (bankAccount.userId !== req.user._id) {
+      return res.status(400).json({
+         errors: {
+            user: "You dont own the bank account you wish to make changes to.",
+         },
+      });
+   }
+
+   const transactionIndex = bankAccount.transactions.findIndex(
+      (transaction) => transaction._id == req.params.id
+   );
+
+   bankAccount.transactions[transactionIndex].type = req.body.type;
+   bankAccount.transactions[transactionIndex].memo = req.body.memo;
+   bankAccount.transactions[transactionIndex].amount = parseInt(
+      req.body.amount
+   );
+
+   try {
+      await bankAccount.save();
+      res.json(bankAccount);
+   } catch (err) {
+      return res.status(500).json({
+         errors: { server: "An error occured. Try again later." },
+      });
+   }
+};
+
+// Delete Transaction
+const deleteTransaction = async (req, res) => {
+   const bankAccount = await BankAccount.findById(req.body.bankId);
+
+   if (!bankAccount) {
+      return res
+         .status(404)
+         .json({ errors: { account: "No bank account with that id." } });
+   }
+
+   if (bankAccount.userId !== req.user._id) {
+      return res.status(400).json({
+         errors: {
+            user: "You dont own the bank account you wish to make changes to.",
+         },
+      });
+   }
+
+   bankAccount.transactions = await bankAccount.transactions.filter(
+      (transaction) => transaction._id != req.params.id
+   );
+
+   try {
+      await bankAccount.save();
+      res.json(bankAccount);
+   } catch (err) {
+      return res.status(500).json({
+         errors: { server: "An error occured. Try again later." },
+      });
+   }
+};
+
 module.exports = {
    createTransaction,
+   updateTransaction,
+   deleteTransaction,
 };

4fb11868d2835d356fc1f8a2484a029e8a6fc932
Blake Yeboah <blakekyeboah@gmail.com>
2021-04-16 14:20:48 +1000

    Added budget property to transaction schema

--- models/transaction.js
+++ models/transaction.js
@@ -4,6 +4,7 @@ const TransactionSchema = new Schema({
    type: String, // Deposit, Withdrawal, Point of Sale
    memo: String,
    amount: Number,
+   budget: { type: String, default: "" },
    date: { type: Date, default: Date.now() },
 });
 

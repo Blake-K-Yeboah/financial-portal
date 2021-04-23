ff9392d98716677f39527180ef50eaef95e05133
Blake Yeboah <blakekyeboah@gmail.com>
2021-04-23 18:56:35 +1000

    Added getTransactionsForHousehold Method

--- controllers/TransactionsController.js
+++ controllers/TransactionsController.js
@@ -4,6 +4,27 @@ const BankAccount = require("../models/bankaccount");
 // Validation Function
 const { validateTransactionInput } = require("../validation/transactions");
 
+// Get Transactions For Household
+const getTransactionsForHousehold = async (req, res) => {
+   if (!req.body.householdId) {
+      return res
+         .status(400)
+         .json({ errors: { household: "Must provide a household id" } });
+   }
+
+   const bankAccounts = await BankAccount.find({
+      linkedTo: req.body.householdId,
+   });
+
+   if (!bankAccounts) {
+      return res.json([]);
+   }
+
+   const transactions = bankAccounts.map((account) => account.transactions);
+
+   res.json(transactions.flat());
+};
+
 // Create a transaction
 const createTransaction = async (req, res) => {
    const { errors, isValid } = validateTransactionInput(req.body);
@@ -121,6 +142,7 @@ const deleteTransaction = async (req, res) => {
 };
 
 module.exports = {
+   getTransactionsForHousehold,
    createTransaction,
    updateTransaction,
    deleteTransaction,

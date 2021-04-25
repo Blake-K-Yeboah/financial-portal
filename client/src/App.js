e6e5da41f919f2ddf4c4b782307c2e3758f5ace3
Blake Yeboah <blakekyeboah@gmail.com>
2021-04-25 21:56:39 +1000

    Setup Router with /register route

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,26 @@
+// Import React Router Stuff
+import { BrowserRouter as Router, Switch } from "react-router-dom";
+
+// Import Pages
+import SignUp from "./components/pages/SignUp";
+
+// Import AuthRoute Utility
+import AuthRoute from "./util/AuthRoute";
+
 function App() {
    return (
-      <div>
-         <h1>Hello!</h1>
-      </div>
+      <>
+         <Router>
+            <Switch>
+               <AuthRoute route="/register">
+                  <SignUp />
+               </AuthRoute>
+               <AuthRoute route="/" isProtected>
+                  <h1>Home</h1>
+               </AuthRoute>
+            </Switch>
+         </Router>
+      </>
    );
 }
 

9b4628a1b557a06620d72498dbcdf33853b7205f
Blake Yeboah <blakekyeboah@gmail.com>
2021-04-25 21:58:01 +1000

    Created AuthRoute Util

---
+++ client/src/util/AuthRoute.js
@@ -0,0 +1,32 @@
+import { Route, Redirect } from "react-router-dom";
+import { useSelector } from "react-redux";
+
+const AuthRoute = (props) => {
+   const isAuthenticated = useSelector((state) => state.auth.isAuthenticated);
+
+   if (props.isProtected) {
+      return (
+         <Route
+            exact
+            path={props.route}
+            render={() => {
+               if (!isAuthenticated) return <Redirect to="/register" />;
+               return <>{props.children}</>;
+            }}
+         />
+      );
+   } else {
+      return (
+         <Route
+            exact
+            path={props.route}
+            render={() => {
+               if (isAuthenticated) return <Redirect to="/" />;
+               return <>{props.children}</>;
+            }}
+         />
+      );
+   }
+};
+
+export default AuthRoute;

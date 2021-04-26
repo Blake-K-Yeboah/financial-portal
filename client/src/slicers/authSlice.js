db75148a33a158a2cd874059127bda87e4f68010
Blake Yeboah <blakekyeboah@gmail.com>
2021-04-26 18:26:53 +1000

    Added errors field and setErrors action

--- client/src/slicers/authSlice.js
+++ client/src/slicers/authSlice.js
@@ -6,15 +6,19 @@ export const authSlice = createSlice({
    initialState: {
       user: null,
       isAuthenticated: false,
+      errors: null,
    },
    reducers: {
       setUser: (state, action) => {
          state.user = action.payload ? jwt_decode(action.payload) : null;
          state.isAuthenticated = action.payload ? true : false;
       },
+      setErrors: (state, action) => {
+         state.errors = action.payload;
+      },
    },
 });
 
-export const { setUser } = authSlice.actions;
+export const { setUser, setErrors } = authSlice.actions;
 
 export default authSlice.reducer;

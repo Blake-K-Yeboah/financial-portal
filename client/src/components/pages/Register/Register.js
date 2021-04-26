f11b87caa7188f90d2d75ca980a0ac2417125299
Blake Yeboah <blakekyeboah@gmail.com>
2021-04-26 18:27:24 +1000

    Finished Register Page

--- client/src/components/pages/Register/Register.js
+++ client/src/components/pages/Register/Register.js
@@ -1,25 +1,44 @@
 import { Box, Flex, Heading, Text } from "@chakra-ui/react";
 
+// Helmet to access head and change title
+import Helmet from "react-helmet";
+
+// Components
+import ErrorAlert from "../../alerts/ErrorAlert";
+import RegisterForm from "./RegisterForm";
+
+// useSelector
+import { useSelector } from "react-redux";
+
 const Register = () => {
+   const errors = useSelector((state) => state.auth.errors);
+
    return (
-      <Flex alignItems="center" justifyContent="center" h="100vh">
-         <Box
-            bg="white"
-            w="27.5%"
-            p={8}
-            h="60%"
-            borderRadius={8}
-            textAlign="center"
-            boxShadow="sm"
-         >
-            <Heading as="h1" size="lg">
-               Register
-            </Heading>
-            <Text fontSize="md" pt={5} color="gray.800">
-               Fill out the following form to register
-            </Text>
-         </Box>
-      </Flex>
+      <>
+         <Helmet>
+            <title>Financial Portal - Register</title>
+         </Helmet>
+         <Flex alignItems="center" justifyContent="center" h="100vh">
+            <Box
+               bg="white"
+               w={["80%", "60%", "40%", "40%", "30%"]}
+               p={8}
+               minH="60%"
+               borderRadius={8}
+               textAlign="center"
+               boxShadow="sm"
+            >
+               <Heading as="h1" size="lg">
+                  Register
+               </Heading>
+               <Text fontSize="md" pt={5} color="gray.800">
+                  Fill out the following form to register
+               </Text>
+               {errors ? <ErrorAlert errors={errors} /> : ""}
+               <RegisterForm />
+            </Box>
+         </Flex>
+      </>
    );
 };
 

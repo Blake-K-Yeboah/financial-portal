5b276de0b723de0b2091ab601fc5aab99eec60bd
Blake Yeboah <blakekyeboah@gmail.com>
2021-04-27 20:10:19 +1000

    Set user if token is present through useEffect hook

--- client/src/App.js
+++ client/src/App.js
@@ -1,3 +1,6 @@
+// useEffect Hook
+import { useEffect } from "react";
+
 // Import React Router Stuff
 import { BrowserRouter as Router, Switch } from "react-router-dom";
 
@@ -7,18 +10,33 @@ import Register from "./components/pages/Register/Register";
 // Import AuthRoute Utility
 import AuthRoute from "./util/AuthRoute";
 
-function App() {
+// useDispatch hook
+import { useDispatch } from "react-redux";
+
+// setUser action
+import { setUser } from "./slicers/authSlice";
+
+const App = () => {
+   // Redux Dispatch
+   const dispatch = useDispatch();
+
+   useEffect(() => {
+      const token = localStorage.getItem("token");
+
+      if (token) {
+         dispatch(setUser(token));
+      }
+   }, []);
+
    return (
-      <>
-         <Router>
-            <Switch>
-               <AuthRoute route="/register">
-                  <Register />
-               </AuthRoute>
-            </Switch>
-         </Router>
-      </>
+      <Router>
+         <Switch>
+            <AuthRoute route="/register">
+               <Register />
+            </AuthRoute>
+         </Switch>
+      </Router>
    );
-}
+};
 
 export default App;

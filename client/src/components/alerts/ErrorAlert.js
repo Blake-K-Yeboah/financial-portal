53bf6e2918576ce32fb927183f034d4c367843d1
Blake Yeboah <blakekyeboah@gmail.com>
2021-04-27 20:10:35 +1000

    Finished Error Alert

--- client/src/components/alerts/ErrorAlert.js
+++ client/src/components/alerts/ErrorAlert.js
@@ -6,27 +6,43 @@ import {
    UnorderedList,
    ListItem,
    Box,
+   CloseButton,
+   Fade,
 } from "@chakra-ui/react";
 
-const ErrorAlert = ({ errors }) => {
+const ErrorAlert = ({ errors, isOpen, closeHandler }) => {
    return (
-      <Alert status="error" textAlign="left" mt={7}>
-         <AlertIcon />
-         <Box pl={3}>
-            <AlertTitle>Errors with your submission!</AlertTitle>
-            <AlertDescription display="block" mt={3}>
-               <UnorderedList>
-                  {errors.map((error) => {
-                     return (
-                        <ListItem fontSize="sm" key={error}>
-                           {error}
-                        </ListItem>
-                     );
-                  })}
-               </UnorderedList>
-            </AlertDescription>
-         </Box>
-      </Alert>
+      <Fade in={isOpen}>
+         {!errors ? (
+            ""
+         ) : (
+            <Alert status="error" textAlign="left" mt={7}>
+               <AlertIcon />
+               <Box pl={3}>
+                  <AlertTitle>Errors with your submission!</AlertTitle>
+                  <AlertDescription display="block" mt={3}>
+                     <UnorderedList>
+                        {errors
+                           ? errors.map((error) => {
+                                return (
+                                   <ListItem fontSize="sm" key={error}>
+                                      {error}
+                                   </ListItem>
+                                );
+                             })
+                           : ""}
+                     </UnorderedList>
+                  </AlertDescription>
+                  <CloseButton
+                     position="absolute"
+                     right="8px"
+                     top="8px"
+                     onClick={closeHandler}
+                  />
+               </Box>
+            </Alert>
+         )}
+      </Fade>
    );
 };
 

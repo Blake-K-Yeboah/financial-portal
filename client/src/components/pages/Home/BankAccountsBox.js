9073f8ce04a8e9b7581f27af3979017f14c19b46
Blake Yeboah <blakekyeboah@gmail.com>
2021-05-05 15:05:44 +1000

    Created Bank Accounts Box

---
+++ client/src/components/pages/Home/BankAccountsBox.js
@@ -0,0 +1,28 @@
+// CHakra Ui Components
+import { Stat, StatHelpText, StatLabel, StatNumber, Box } from '@chakra-ui/react';
+
+// useSelector
+import { useSelector } from "react-redux";
+
+const BankAccountsBox = () => {
+    const user = useSelector(state => state.auth.user);
+    const userBankAccounts = useSelector(state => state.bankAccounts.bankAccounts.filter(account => account.userId === user._id));
+
+    const userCreatedAt = user.createdAt.substring(0, 9).replaceAll('-', '/');
+
+    const reverseDate = string => {
+        return `${userCreatedAt.substring(8, 9)}/${userCreatedAt.substring(5, 7)}/${userCreatedAt.substring(0, 4)}`;
+    }
+
+    return (
+        <Box w="100%" h="100%" bg="white" boxShadow="sm" p={8} borderRadius={8}>
+            <Stat>
+                <StatLabel>Bank Accounts</StatLabel>
+                <StatNumber>{userBankAccounts.length}</StatNumber>
+                <StatHelpText>Since {reverseDate(userCreatedAt)}</StatHelpText>
+            </Stat>
+        </Box>
+    )
+}
+
+export default BankAccountsBox;

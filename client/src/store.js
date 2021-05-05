53a9e1b95260e9bf4169f2df9e8ccc95665cb6b5
Blake Yeboah <blakekyeboah@gmail.com>
2021-05-05 15:04:37 +1000

    Added bankAccounts slice

--- client/src/store.js
+++ client/src/store.js
@@ -1,8 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
 import authReducer from "./slicers/authSlice";
+import bankAccountsReducer from "./slicers/bankAccountsSlice";
 
 export default configureStore({
    reducer: {
       auth: authReducer,
+      bankAccounts: bankAccountsReducer,
    },
 });

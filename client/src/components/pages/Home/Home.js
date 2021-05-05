26d81a5ed44138370a12359e45e9056bd7dbfbc8
Blake Yeboah <blakekyeboah@gmail.com>
2021-05-05 15:05:12 +1000

    Added Fetch Bank Accounts Request to Home Page

--- client/src/components/pages/Home/Home.js
+++ client/src/components/pages/Home/Home.js
@@ -1,6 +1,9 @@
 // Chakra UI Components
 import { Container, Grid, GridItem } from "@chakra-ui/react";
 
+// useEffect Hook
+import { useEffect } from "react";
+
 // Helmet to Access Head
 import { Helmet } from "react-helmet";
 
@@ -8,7 +11,36 @@ import { Helmet } from "react-helmet";
 import Navbar from "../../layout/Navbar";
 import Sidebar from "../../layout/Sidebar";
 
+// useDispatch
+import { useDispatch } from "react-redux";
+
+// Axios for HTTP Requests
+import axios from 'axios';
+
+// Store Actions
+import { setBankAccounts } from '../../../slicers/bankAccountsSlice';
+import BankAccountsBox from "./BankAccountsBox";
+
 const Home = () => {
+
+   const dispatch = useDispatch();
+
+   useEffect(() => {
+      const fetchBankAccounts = async () => {
+         try {
+            const res = await axios.get("/api/bankaccounts", { headers: {
+               'Authorization': `Bearer ${localStorage.getItem('token')}`
+            }});
+            dispatch(setBankAccounts(res.data));
+         } catch(err) {
+            alert("An error occured");
+            console.error(err);
+         }
+      }
+
+      fetchBankAccounts();
+   }, [dispatch]);
+
    return (
       <>
          <Helmet>
@@ -26,7 +58,9 @@ const Home = () => {
                   <GridItem rowSpan={4} colSpan={2}>
                      <Sidebar page="home" />
                   </GridItem>
-                  <GridItem colSpan={2} bg="green.100" />
+                  <GridItem colSpan={2}>
+                     <BankAccountsBox />
+                  </GridItem>
                   <GridItem colSpan={2} bg="green.100" />
                   <GridItem colSpan={2} bg="green.100" />
                   <GridItem colSpan={6} rowSpan={3} bg="green.300" />

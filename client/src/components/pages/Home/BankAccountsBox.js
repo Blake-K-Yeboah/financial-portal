67b8c0c1bb9fd5018a503404c84da071b7984b7b
Blake Yeboah <blakekyeboah@gmail.com>
2021-05-05 18:42:32 +1000

    Fixed bug with reversing date

--- client/src/components/pages/Home/BankAccountsBox.js
+++ client/src/components/pages/Home/BankAccountsBox.js
@@ -1,28 +1,42 @@
 // CHakra Ui Components
-import { Stat, StatHelpText, StatLabel, StatNumber, Box } from '@chakra-ui/react';
+import {
+   Stat,
+   StatHelpText,
+   StatLabel,
+   StatNumber,
+   Box,
+} from "@chakra-ui/react";
 
 // useSelector
 import { useSelector } from "react-redux";
 
 const BankAccountsBox = () => {
-    const user = useSelector(state => state.auth.user);
-    const userBankAccounts = useSelector(state => state.bankAccounts.bankAccounts.filter(account => account.userId === user._id));
+   const user = useSelector((state) => state.auth.user);
 
-    const userCreatedAt = user.createdAt.substring(0, 9).replaceAll('-', '/');
+   const userBankAccounts = useSelector((state) =>
+      state.bankAccounts.bankAccounts.filter(
+         (account) => account.userId === user._id
+      )
+   );
 
-    const reverseDate = string => {
-        return `${userCreatedAt.substring(8, 9)}/${userCreatedAt.substring(5, 7)}/${userCreatedAt.substring(0, 4)}`;
-    }
+   const userCreatedAt = user.createdAt.substring(0, 9).replaceAll("-", "/");
 
-    return (
-        <Box w="100%" h="100%" bg="white" boxShadow="sm" p={8} borderRadius={8}>
-            <Stat>
-                <StatLabel>Bank Accounts</StatLabel>
-                <StatNumber>{userBankAccounts.length}</StatNumber>
-                <StatHelpText>Since {reverseDate(userCreatedAt)}</StatHelpText>
-            </Stat>
-        </Box>
-    )
-}
+   const reverseDate = () => {
+      return `${userCreatedAt.substring(8, 9)}/${userCreatedAt.substring(
+         5,
+         7
+      )}/${userCreatedAt.substring(0, 4)}`;
+   };
+
+   return (
+      <Box w="100%" h="100%" bg="white" boxShadow="sm" p={8} borderRadius={8}>
+         <Stat>
+            <StatLabel>Bank Accounts</StatLabel>
+            <StatNumber>{userBankAccounts.length}</StatNumber>
+            <StatHelpText>Since {reverseDate()}</StatHelpText>
+         </Stat>
+      </Box>
+   );
+};
 
 export default BankAccountsBox;

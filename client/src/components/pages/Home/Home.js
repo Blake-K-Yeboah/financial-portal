40ae0b40bc97940d40870fba8fcfa4febc5abf53
Blake Yeboah <blakekyeboah@gmail.com>
2021-05-06 16:55:23 +1000

    Updated Amount of Rows in Grid

--- client/src/components/pages/Home/Home.js
+++ client/src/components/pages/Home/Home.js
@@ -65,11 +65,11 @@ const Home = () => {
             <Container maxW="container.xl" h="75vh" mt={16}>
                <Grid
                   h="100%"
-                  templateRows="repeat(4, 1fr)"
+                  templateRows="repeat(5, 1fr)"
                   templateColumns="repeat(8, 1fr)"
                   gap={4}
                >
-                  <GridItem rowSpan={4} colSpan={2}>
+                  <GridItem rowSpan={5} colSpan={2}>
                      <Sidebar page="home" />
                   </GridItem>
                   <GridItem colSpan={2}>
@@ -79,7 +79,7 @@ const Home = () => {
                      <TransactionsBox />
                   </GridItem>
                   <GridItem colSpan={2} bg="green.100" />
-                  <GridItem colSpan={6} rowSpan={3} bg="green.300" />
+                  <GridItem colSpan={6} rowSpan={4} bg="green.300" />
                </Grid>
             </Container>
          </>

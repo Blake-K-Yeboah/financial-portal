4f9dbb2b679e71b44cf3953d2802ac7122107c58
Blake Yeboah <blakekyeboah@gmail.com>
2021-05-20 17:55:35 +1000

    Fixed CreateBankAccount Modal Props

--- client/src/components/pages/BankAccounts/BankAccounts.js
+++ client/src/components/pages/BankAccounts/BankAccounts.js
@@ -19,12 +19,8 @@ import { useDispatch, useSelector } from "react-redux";
 // useHistory hook to redirect user
 import { useHistory } from "react-router";
 
-// Axios for HTTP Requests
-import axios from "axios";
-
-// Store Actions
-import { setBankAccounts } from "../../../slicers/bankAccountsSlice";
-import { setUser } from "../../../slicers/authSlice";
+// Api Requests
+import { fetchBankAccounts } from "../../../util/apiRequests";
 
 const BankAccounts = () => {
    const dispatch = useDispatch();
@@ -34,25 +30,7 @@ const BankAccounts = () => {
    const token = useSelector((state) => state.auth.token);
 
    useEffect(() => {
-      axios
-         .get("/api/bankaccounts", {
-            headers: {
-               Authorization: `Bearer ${token}`,
-            },
-         })
-         .then((res) => {
-            dispatch(setBankAccounts(res.data));
-         })
-         .catch((err) => {
-            if (err.response.status === 401) {
-               alert("Session expired. Please login again");
-               setTimeout(() => {
-                  localStorage.removeItem("token");
-                  dispatch(setUser(null));
-                  history.push("/login");
-               });
-            }
-         });
+      fetchBankAccounts(token, dispatch, history);
    }, [dispatch, history, token]);
 
    const { isOpen, onOpen, onClose } = useDisclosure();
@@ -74,7 +52,7 @@ const BankAccounts = () => {
                   </GridItem>
                </Grid>
             </Container>
-            <CreateBankAccount isOpen={isOpen} onClose={onClose} />
+            <CreateBankAccount modalIsOpen={isOpen} modalOnClose={onClose} />
          </>
       </>
    );

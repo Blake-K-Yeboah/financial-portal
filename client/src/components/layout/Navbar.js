51f663ffbc315e932d8616807a7ad6de53939427
Blake Yeboah <blakekyeboah@gmail.com>
2021-06-03 16:18:15 +1000

    Added Menu Icon and Mobile Sidebar

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -1,9 +1,18 @@
-import { Box, Button, chakra, Container, Heading } from "@chakra-ui/react";
+import {
+   Box,
+   Button,
+   chakra,
+   Container,
+   Heading,
+   useDisclosure,
+} from "@chakra-ui/react";
+import { HamburgerIcon } from "@chakra-ui/icons";
 import { useDispatch } from "react-redux";
 import { useHistory } from "react-router";
 import { setUser } from "../../slicers/authSlice";
+import MobileSidebar from "./MobileSidebar";
 
-const Navbar = () => {
+const Navbar = ({ page }) => {
    const dispatch = useDispatch();
 
    const history = useHistory();
@@ -13,6 +22,8 @@ const Navbar = () => {
       dispatch(setUser(null));
       history.push("/login");
    };
+
+   const { isOpen, onOpen, onClose } = useDisclosure();
 
    return (
       <Box w="100vw" h="80px" bgColor="white" boxShadow="sm">
@@ -26,9 +37,22 @@ const Navbar = () => {
             <Heading as="h2" size="lg">
                Financial <chakra.span color="green.400">Portal</chakra.span>
             </Heading>
-            <Button colorScheme="red" variant="outline" onClick={logoutHandler}>
+            <Button
+               colorScheme="red"
+               variant="outline"
+               onClick={logoutHandler}
+               display={{ base: "none", md: "block" }}
+            >
                Logout
             </Button>
+            <HamburgerIcon
+               display={{ base: "block", md: "none" }}
+               color="grey.700"
+               w={6}
+               h={6}
+               onClick={onOpen}
+            />
+            <MobileSidebar isOpen={isOpen} onClose={onClose} page={page} />
          </Container>
       </Box>
    );

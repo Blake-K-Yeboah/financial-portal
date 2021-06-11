cf9fb9109beb873cfa3966a03602c01970e798b3
Blake Yeboah <blakekyeboah@gmail.com>
2021-06-11 16:42:38 +1000

    Added Delete Account Dialog

--- client/src/components/pages/IndividualBankAccount/BankAccountDetails.js
+++ client/src/components/pages/IndividualBankAccount/BankAccountDetails.js
@@ -7,7 +7,11 @@ import {
    Spinner,
    Button,
    Flex,
+   useDisclosure,
 } from "@chakra-ui/react";
+
+// Components
+import DeleteAccountDialog from "../BankAccounts/DeleteAccountDialog";
 
 const BankAccountDetails = ({ bankAccount }) => {
    const date = bankAccount ? new Date(bankAccount.createdAt) : null;
@@ -22,48 +26,62 @@ const BankAccountDetails = ({ bankAccount }) => {
         }:${date.getMinutes()} ${date.getHours() > 12 ? "PM" : "AM"}`
       : "";
 
+   const { isOpen, onOpen, onClose } = useDisclosure();
+
    return (
-      <Box
-         w="100%"
-         h="100%"
-         maxH="100%"
-         bg="white"
-         boxShadow="sm"
-         position="relative"
-         p={8}
-         borderRadius={8}
-      >
-         <Heading as="h3" size="md" color="gray.600" mb={4}>
-            Bank Account Details
-         </Heading>
-         {bankAccount ? (
-            <>
-               <Text fontSize={16} color="gray.600">
-                  <chakra.span fontWeight="medium">Name: </chakra.span>
-                  {bankAccount.name}
-               </Text>
-               <Text fontSize={16} color="gray.600" mt={3}>
-                  <chakra.span fontWeight="medium">Type: </chakra.span>
-                  {bankAccount.type[0].toUpperCase() +
-                     bankAccount.type.substring(1, bankAccount.type.length)}
-               </Text>
-               <Text fontSize={16} color="gray.600" mt={3}>
-                  <chakra.span fontWeight="medium">Created At: </chakra.span>
-                  {dateDisplay}
-               </Text>
-               <Flex mt={6}>
-                  <Button colorScheme="green" size="sm">
-                     Edit Details
-                  </Button>
-                  <Button colorScheme="red" size="sm" ml={4}>
-                     Delete Account
-                  </Button>
-               </Flex>
-            </>
-         ) : (
-            <Spinner color="green.400" />
-         )}
-      </Box>
+      <>
+         <DeleteAccountDialog
+            bankAccount={bankAccount}
+            isOpen={isOpen}
+            onClose={onClose}
+         />
+         <Box
+            w="100%"
+            h="100%"
+            maxH="100%"
+            bg="white"
+            boxShadow="sm"
+            position="relative"
+            p={8}
+            borderRadius={8}
+         >
+            <Heading as="h3" size="md" color="gray.600" mb={4}>
+               Bank Account Details
+            </Heading>
+            {bankAccount ? (
+               <>
+                  <Text fontSize={16} color="gray.600">
+                     <chakra.span fontWeight="medium">Name: </chakra.span>
+                     {bankAccount.name}
+                  </Text>
+                  <Text fontSize={16} color="gray.600" mt={3}>
+                     <chakra.span fontWeight="medium">Type: </chakra.span>
+                     {bankAccount.type[0].toUpperCase() +
+                        bankAccount.type.substring(1, bankAccount.type.length)}
+                  </Text>
+                  <Text fontSize={16} color="gray.600" mt={3}>
+                     <chakra.span fontWeight="medium">Created At: </chakra.span>
+                     {dateDisplay}
+                  </Text>
+                  <Flex mt={6}>
+                     <Button colorScheme="green" size="sm">
+                        Edit Details
+                     </Button>
+                     <Button
+                        colorScheme="red"
+                        size="sm"
+                        ml={4}
+                        onClick={onOpen}
+                     >
+                        Delete Account
+                     </Button>
+                  </Flex>
+               </>
+            ) : (
+               <Spinner color="green.400" />
+            )}
+         </Box>
+      </>
    );
 };
 

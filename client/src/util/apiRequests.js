11e39137f1a6e26b069441f53718e09d57e2b8aa
Blake Yeboah <blakekyeboah@gmail.com>
2021-06-24 13:43:55 +1000

    Created fetchHouseholdData Request and Made All Requests Asynchronous

--- client/src/util/apiRequests.js
+++ client/src/util/apiRequests.js
@@ -4,23 +4,51 @@ import axios from "axios";
 // Store Actions
 import { setBankAccounts } from "../slicers/bankAccountsSlice";
 import { setUser } from "../slicers/authSlice";
+import { setHousehold, setTransactions } from "../slicers/householdSlice";
 
-export const fetchBankAccounts = (token, dispatch, history) => {
-   axios
-      .get("/api/bankaccounts", {
+export const fetchBankAccounts = async (token, dispatch, history) => {
+   try {
+      const res = await axios.get("/api/bankaccounts", {
          headers: {
             Authorization: `Bearer ${token}`,
          },
-      })
-      .then((res) => {
-         dispatch(setBankAccounts(res.data));
-      })
-      .catch((err) => {
-         if (err.response.status === 401) {
-            alert("Session expired. Please login again");
-            localStorage.removeItem("token");
-            dispatch(setUser(null));
-            history.push("/login");
-         }
       });
+      dispatch(setBankAccounts(res.data));
+   } catch (err) {
+      if (err.response.status === 401) {
+         alert("Session expired. Please login again");
+         localStorage.removeItem("token");
+         dispatch(setUser(null));
+         history.push("/login");
+      }
+   }
+};
+
+export const fetchHouseholdData = async (token, dispatch, history) => {
+   try {
+      const householdRes = await axios.get("/api/households/", {
+         headers: {
+            Authorization: `Bearer ${token}`,
+         },
+      });
+      const household = householdRes.data;
+      dispatch(setHousehold(household));
+
+      const transactionsRes = await axios.get("/api/transactions/", {
+         headers: {
+            Authorization: `Bearer ${token}`,
+         },
+         data: {
+            householdId: household._id,
+         },
+      });
+      dispatch(setTransactions(transactionsRes.data));
+   } catch (err) {
+      if (err.response.status === 401) {
+         alert("Session expired. Please login again");
+         localStorage.removeItem("token");
+         dispatch(setUser(null));
+         history.push("/login");
+      }
+   }
 };

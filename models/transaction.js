22ef75b54d7538e6a2a2ed2f87b0d1f0533b6362
Blake Yeboah <blakekyeboah@gmail.com>
2021-06-25 12:40:03 +1000

    Removed Budget Property From Transaction Model

--- models/transaction.js
+++ models/transaction.js
@@ -4,7 +4,6 @@ const TransactionSchema = new Schema({
    type: String, // Deposit, Withdrawal, Point of Sale
    memo: String,
    amount: Number,
-   budget: { type: String, default: "" },
    date: { type: Date, default: Date.now() },
 });
 

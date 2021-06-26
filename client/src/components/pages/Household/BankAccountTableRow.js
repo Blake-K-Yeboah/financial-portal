9802bda0ba475af614075591785778aaea0142dc
Blake Yeboah <blakekyeboah@gmail.com>
2021-06-26 20:08:27 +1000

    Removed Delete Account Button

--- client/src/components/pages/Household/BankAccountTableRow.js
+++ client/src/components/pages/Household/BankAccountTableRow.js
@@ -2,12 +2,9 @@
 import { useHistory } from "react-router-dom";
 
 // Chakra UI Components
-import { Tr, Td, Flex, Button } from "@chakra-ui/react";
-import { useSelector } from "react-redux";
+import { Tr, Td, Button } from "@chakra-ui/react";
 
 const BankAccountTableRow = ({ account }) => {
-   const userRole = useSelector((state) => state.auth.user.role);
-
    let history = useHistory();
 
    const redirectUser = (route) => {
@@ -33,25 +30,16 @@ const BankAccountTableRow = ({ account }) => {
          <Td>{capitalise(account.type)}</Td>
          <Td>{dateOutput}</Td>
          <Td>
-            <Flex>
-               <Button
-                  colorScheme="green"
-                  size="sm"
-                  onClick={redirectUser.bind(
-                     this,
-                     `/bank-accounts/${account._id}`
-                  )}
-               >
-                  View Account
-               </Button>
-               {userRole === "owner" ? (
-                  <Button colorScheme="red" variant="outline" size="sm" ml={4}>
-                     Delete Account
-                  </Button>
-               ) : (
-                  ""
+            <Button
+               colorScheme="green"
+               size="sm"
+               onClick={redirectUser.bind(
+                  this,
+                  `/bank-accounts/${account._id}`
                )}
-            </Flex>
+            >
+               View Account
+            </Button>
          </Td>
       </Tr>
    );
